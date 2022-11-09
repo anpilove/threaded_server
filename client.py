3207532b7381ac3acc3583ea56af304e1fa89589
kirillanpilov <anpilov.kirill2002@gmail.com>
2022-11-10 02:00:57 +0300

    Add comments server.py and client.py

--- client.py
+++ client.py
@@ -6,21 +6,23 @@ import time
 class Client:
 
     def __init__(self):
-        self.sock = socket.socket()  # создаем сокет из класса сокет
+        self.sock = socket.socket()  # object from class socket
         self.localhost = ''
-        self.port = int(input("Port to connect: "))  # port 0 до 65535
+        self.port = int(input("Port to connect: "))  # port 0 to 65535
         self.run()
 
     def run(self):
-        self.sock.connect(('', self.port))
-        threading.Thread(target=self.recv_data).start()
+        self.sock.connect(('', self.port))  # connect to server
+        threading.Thread(target=self.recv_data).start()  # branch process
+        # (first - sending connections)
+        # (second - receive data from server)
         while True:
             msg = input("Message: ")
-            self.sock.send(msg.encode())
+            self.sock.send(msg.encode())  # send data
 
     def recv_data(self):
         while True:
-            data = self.sock.recv(1024).decode()
+            data = self.sock.recv(1024).decode()  # get data
             print(data)
 
 
--- server.py
+++ server.py
@@ -19,18 +19,21 @@ class Server:
             conn, addr = self.sock.accept()  # accepting connection from clients in while
             self.clients.append(conn)
             threading.Thread(target=self.data_client, args=[conn, addr]).start()
-            # branch process (first - accepting connections)
+            # branch process
+            # (first - accepting connections)
             # (second - receive data in data_clients)
 
     def data_client(self, conn, addr):
         while True:
-            data = conn.recv(1024).decode()
+            data = conn.recv(1024).decode()  # receive data from clients
             # conn.send("Message delivered".encode())
-            print(f"Message from {addr[0]} is {data}")
+            print(f"Message from {addr[0]} is {data}")   # print data from clients
             if not data:
                 break
             for sock in self.clients:
+                # get all socket objects to send data to all clients which are connected
                 if sock != conn:
+                    # send data except itself
                     sock.send(f"Message from {addr[0]} is {data}".encode())
 
         conn.close()
