1166cb645d8a48a70f32e253713a35418fd6e998
kirillanpilov <anpilov.kirill2002@gmail.com>
2022-11-10 14:40:10 +0300

    Final commit 1.0

--- parallelism.py
+++ parallelism.py
@@ -1,12 +1,53 @@
+import csv
 import threading
 from time import *
-
-def proc(n):
-    sleep(int(n))
-    print("Процесс", n)
+from multiprocessing import Process, Pool
+import numpy as np
 
 
-p1 = threading.Thread(target=proc, name="t1", args=["1"])
-p2 = threading.Thread(target=proc, name="t2", args=["2"])
-p1.start()
-p2.start()
+def print_matrix(matrix):
+    for i in matrix:
+        print(*i, end='\n')
+
+
+class Multiplication:
+
+    def __init__(self, size1=None, size2=None):
+        self.matrix1 = self.read_csvmatrix('matrix1.csv')
+        self.matrix2 = self.read_csvmatrix('matrix2.csv')
+        self.final = np.zeros((len(self.matrix1),len(self.matrix2[0])))
+        self.multi()
+
+    def read_csvmatrix(self, name_file):
+        matrix = []
+        with open(name_file, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file, delimiter=',')
+            for i in reader:
+                i = [int(j) for j in i]
+                matrix.append(i)
+
+        return matrix
+
+    def multi(self):
+        thread = []
+        for i in range(self.final.shape[0]):
+            for j in range(self.final.shape[1]):
+                t = threading.Thread(target=self.element, args=[(i,j)])
+                t.start()
+                thread.append(t)
+
+            for t in thread:
+                t.join()
+
+        print_matrix(self.final)
+
+    def element(self, index):
+        i, j = index
+        for k in range(len(self.matrix2)):
+            self.final[i][j] += self.matrix1[i][k] * self.matrix2[k][j]
+
+
+
+
+task1 = Multiplication()
+
